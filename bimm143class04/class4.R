b0b96980e488043380a1e28ec352528d6b271ee3
Lily Huynh <lphuynh@ucsd.edu>
2025-02-25 11:42:34 -0800

    class 4 & 5

---
+++ bimm143class04/class4.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: "Lily Huynh (PID:A16929651)"
+#' date: "January 16, 2025"
+#' ---
+
+x <-1:50
+plot(x, sin(x))
+
+# Make this nicer
+plot(x, sin(x), col="blue", type="l", lwd=3)
